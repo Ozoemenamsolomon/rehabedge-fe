7f032e9077c54ea969260fd542df1cfad865d613
Ozoemenamsolomon <ozoemenamsolomon@yahoo.com>
2021-05-23 09:08:35 +0200

    add header animate on scroll, still need to figure out styling the header on data-active attribute true

--- src/components/header.js
+++ src/components/header.js
@@ -1,48 +1,72 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Link } from "gatsby";
 import HamburgerMenu from "./HamburgerMenu";
 import { StaticImage } from "gatsby-plugin-image";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#2f4293`,
-      marginBottom: `1.45rem`,
-      position: `fixed`,
-      minWidth: `100%`,
-      top: 0,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  // determined if page has scrolled and if the view is on mobile
+  const [scrolled, setScrolled] = useState(false);
+
+  // change state on scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      if (isScrolled !== scrolled) {
+        setScrolled(!scrolled);
+      }
+    };
+
+    document.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      // clean up the event handler when the component unmounts
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrolled]);
+  return (
+    <header
+      data-active={scrolled}
       style={{
-        margin: `0 auto`,
-        maxWidth: `90%`,
-        padding: `1rem 0rem`,
-        display: `flex`,
-        justifyContent: `space-between`,
-        alignItems: `center`,
+        background: `#2f4293`,
+        marginBottom: `1.45rem`,
+        position: `fixed`,
+        minWidth: `100%`,
+        top: 0,
+        zIndex: 2,
       }}
     >
-      <Link
-        to="/"
+      <div
         style={{
-          color: `white`,
-          textDecoration: `none`,
+          margin: `0 auto`,
+          maxWidth: `90%`,
+          padding: `1rem 0rem`,
+          display: `flex`,
+          justifyContent: `space-between`,
+          alignItems: `center`,
         }}
       >
-        <StaticImage
-          src="../images/rehabedge-icon.png"
-          width={211.65354331}
-          quality={95}
-          formats={["AUTO", "WEBP", "AVIF"]}
-          alt="A Gatsby astronaut"
-        />
-      </Link>
-      <HamburgerMenu></HamburgerMenu>
-    </div>
-  </header>
-);
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          <StaticImage
+            src="../images/rehabedge-icon.png"
+            width={211.65354331}
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="RehabEdge Logo | Home"
+          />
+        </Link>
+        <HamburgerMenu></HamburgerMenu>
+      </div>
+    </header>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
