400d6721f6b6e9593beb2289de01f1e14df8c17b
Ozoemenamsolomon <ozoemenamsolomon@yahoo.com>
2021-06-01 23:31:51 +0200

    header conflit resolves

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `RehabEdge Blog`,
-    description: `RehabEdge Blog`,
-    author: `Ozoemenam Solomon Obinna | https://solozo.page`,
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
   flags: {
     DEV_SSR: true,
@@ -23,11 +23,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `RehabEdge Blog`,
-        short_name: `RehabEdge`,
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
         start_url: `/`,
-        background_color: `#2f4293`,
-        theme_color: `#2f4293`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
