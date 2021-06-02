2b819d7447a53bb73e28e76f1439ae62f1e0b7b2
Ozoemenamsolomon <ozoemenamsolomon@yahoo.com>
2021-06-02 15:11:59 +0200

    install & import font, clear layout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,8 @@
  *
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
-
+import "@fontsource/montserrat";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/900.css";
+import "@fontsource/montserrat/800.css";
 // You can delete this file if you're not using it
