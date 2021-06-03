e347158e6942f2efb909aa6c0a4b657732690b16
Ozoemenamsolomon <ozoemenamsolomon@yahoo.com>
2021-06-03 20:07:24 +0200

    add pageWrapper in 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,17 @@
-import * as React from "react"
+import * as React from "react";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import PageWrapper from "../components/PageWrapper";
+import Seo from "../components/seo";
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <PageWrapper>
+      <Seo title="404: Not found" />
+      <h1>404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </PageWrapper>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
